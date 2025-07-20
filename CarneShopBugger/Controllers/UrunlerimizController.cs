1d952ee618fc15595c262803c98c81748e637688
emreyyulusoy <eulusoy139@gmail.com>
2025-07-21 01:06:44 +0300

    21

---
+++ CarneShopBugger/Controllers/UrunlerimizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarneShopBugger.Controllers
+{
+    public class UrunlerimizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
